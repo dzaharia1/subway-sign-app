63d29db7b9f10f0c47986c0feb64aa81b572028d
Dan Zaharia <zaharia.danny@gmail.com>
2022-09-03 17:08:22 -0400

    fix bugs showing express trains in mockup

--- components/sign-mockup/SignMockup.jsx
+++ components/sign-mockup/SignMockup.jsx
@@ -6,8 +6,11 @@ const SignMockup = ({ arrivals, localOptions, editMode, signId }) => {
         {arrivals.map((arrival, i) => 
         (<li className={styles.arrival} key={i}>
                 <p className={styles.index} style={{display: localOptions.rotating ? "block" : "none"}}>{ i + 1 }</p>
-                <div className={`${styles.route} line--${arrival.routeId[0]} ${(arrival.routeId[1] === 'X') ? styles.express : null}`}>
-                    <p>{arrival.routeId[0]}</p>
+                <div className={`${styles.route} line--${(arrival.routeId.length == 2 && arrival.routeId[1] == 'S') ? 'S' : arrival.routeId[0]}
+                        ${(arrival.routeId[1] === 'X') ? styles.express : null}`}>
+                    <p>
+                        {(arrival.routeId.length == 2) ? ((arrival.routeId[1] == 'X') ? arrival.routeId[0] : arrival.routeId[1]) : (arrival.routeId[0])}
+                    </p>
                 </div>
                 <h3 className={styles.headsign}>{arrival.headsign}</h3>
                 <p className={ (arrival.minutesUntil <= localOptions.warn_time ) ? styles.warning : "" }>{arrival.minutesUntil} min</p>
