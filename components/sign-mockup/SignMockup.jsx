ba7fe576f77d2e8b929224626a75653dd42395d6
Dan Zaharia <zaharia.danny@gmail.com>
2022-09-03 16:58:22 -0400

    create the save notification indicator

--- components/sign-mockup/SignMockup.jsx
+++ components/sign-mockup/SignMockup.jsx
@@ -2,23 +2,7 @@ import { useEffect, useState } from 'react';
 import styles from './SignMockup.module.scss';
 
 const SignMockup = ({ arrivals, localOptions, editMode, signId }) => {
-    const [mockupLoading, setMockupLoading] = useState(false);
-
-    // useEffect(() => {
-    //     setMockupLoading(true);
-
-    //     fetch(`https://subway-arrivals.herokuapp.com/sign/${signId}`)
-    //     .then(res => res.json())
-    //     .then((data) => {
-    //         setArrivals([data[1], data[2]]);
-    //         setMockupLoading(false);
-    //     });
-    // }, [localOptions]);
-    // useEffect(() => {
-
-    // }, [arrivals])
-
-    return <ul className={ `${styles['sign-mockup']} ${mockupLoading ? styles['sign-mockup--loading'] : ''} ${editMode ? styles['sign-mockup--edit-mode'] : ''}` }>
+    return <ul className={ `${styles['sign-mockup']} ${editMode ? styles['sign-mockup--edit-mode'] : ''}` }>
         {arrivals.map((arrival, i) => 
         (<li className={styles.arrival} key={i}>
                 <p className={styles.index} style={{display: localOptions.rotating ? "block" : "none"}}>{ i + 1 }</p>
